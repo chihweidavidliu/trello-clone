10d63757898fba7429b5b781880ec076710532c0
David Liu <chihweiliu1993@gmail.com>
2023-09-03 12:40:19 +0100

    simplify config

--- server/src/db/knexfile.ts
+++ server/src/db/knexfile.ts
@@ -1,46 +1,13 @@
-import type { Knex } from "knex";
 import { config as appConfig } from "../config";
 
 // Update with your config settings.
 
-const config: { [key: string]: Knex.Config } = {
-  development: {
-    client: "pg",
-    connection: {
-      connectionString: appConfig().databaseUrl,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
+module.exports = {
+  client: "pg",
+  connection: {
+    connectionString: appConfig().databaseUrl,
   },
-
-  staging: {
-    client: "pg",
-    connection: {
-      connectionString: "",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
-  production: {
-    client: "pg",
-    connection: {
-      connectionString: "",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
+  migrations: {
+    tableName: "knex_migrations",
   },
 };
-
-module.exports = config;
