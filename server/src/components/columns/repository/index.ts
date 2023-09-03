6bfc4969643cbbfee99333fe93d3ea3260da25b8
David Liu <chihweiliu1993@gmail.com>
2023-09-03 22:21:05 +0100

    remove dead code

--- server/src/components/columns/repository/index.ts
+++ server/src/components/columns/repository/index.ts
@@ -19,26 +19,6 @@ export class ColumnsRepository {
     this.dbContext = dbContext;
   }
 
-  // async getTicketById(id: string): Promise<TicketDTO | null> {
-  //   const rawTicket = await this.dbContext
-  //     .table("ticket")
-  //     .select("*")
-  //     .where("id", id)
-  //     .first();
-
-  //   if (!rawTicket) return null;
-
-  //   const assignedToUsers = await this.dbContext
-  //     .table("ticket_assigned_to_user")
-  //     .select("*")
-  //     .where("ticketId", rawTicket?.id);
-
-  //   return {
-  //     ...rawTicket,
-  //     assignedToUsers,
-  //   };
-  // }
-
   async getColumnsById(ids: string[]): Promise<ColumnEntity[]> {
     const rawColumnns = await this.dbContext
       .table("board_column")
@@ -103,28 +83,4 @@ export class ColumnsRepository {
       throw new InternalServerError("Error saving columns");
     }
   }
-
-  // async updateTicketsOrder(tickets: TicketInitializer[]): Promise<TicketDTO[]> {
-  //   const updatedRows = await this.dbContext
-  //     .table("ticket")
-  //     .insert(tickets)
-  //     .onConflict("id")
-  //     .merge()
-  //     .returning("*");
-
-  //   const assignedToUsers = await this.dbContext
-  //     .table("ticket_assigned_to_user")
-  //     .select("*")
-  //     .where(
-  //       "ticketId",
-  //       updatedRows.map((row) => row.id)
-  //     );
-
-  //   const assignedToUsersByTicketId = groupBy(assignedToUsers, "ticketId");
-
-  //   return updatedRows.map((rawTicket) => ({
-  //     ...rawTicket,
-  //     assignedToUsers: assignedToUsersByTicketId[rawTicket.id],
-  //   }));
-  // }
 }
