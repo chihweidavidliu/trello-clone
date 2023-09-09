3f1467e4e58498279871d228d21a3b07bb2f0ea1
David Liu <chihweiliu1993@gmail.com>
2023-09-09 19:54:00 +0100

    ticket mapper

---
+++ server/src/components/columns/domain/ticket.mapper.ts
@@ -0,0 +1,31 @@
+import { TicketDTO } from "shared-utils";
+import {
+  BoardColumnId,
+  Ticket,
+  TicketAssignedToUser,
+  TicketId,
+} from "../../../db/generated-types";
+import { TicketEntity } from "./ticket.entity";
+import { Mapper } from "../../../types/domain/Mapper";
+
+export interface TicketMapper extends Mapper<TicketEntity, Ticket, TicketDTO> {}
+
+export const ticketMapper: TicketMapper = {
+  toPersistence(ticket: TicketEntity): Ticket {
+    const props = ticket.props;
+    return {
+      ...props,
+      id: props.id as TicketId,
+      columnId: props.columnId as BoardColumnId,
+    };
+  },
+  toDTO(ticket: TicketEntity): TicketDTO {
+    return ticket.props;
+  },
+  toDomain(
+    rawTicket: Ticket,
+    assignedToUsers: TicketAssignedToUser[]
+  ): TicketEntity {
+    return TicketEntity.create({ ...rawTicket, assignedToUsers });
+  },
+};
